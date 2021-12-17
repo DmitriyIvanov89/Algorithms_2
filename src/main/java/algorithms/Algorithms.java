8bacd05b3093c79e6d68f9e1af0287ff6c4e7eaa
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-12-17 10:24:16 +0300

    added search algorithms

--- src/main/java/algorithms/Algorithms.java
+++ src/main/java/algorithms/Algorithms.java
@@ -2,7 +2,8 @@ package algorithms;
 
 public class Algorithms {
     public static void main(String[] args) {
-        
+
+
     }
 
     private static long getRecursiveFactorial(int n) {
@@ -31,6 +32,47 @@ public class Algorithms {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
         return arr[n];
+    }
+
+    // O(n)
+    private static int linearSearch(int[] array, int key) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // O(Log2n)
+    // Только отсортированный массив
+    private static int binarySearch(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (key > array[middle]) {
+                left = middle + 1;
+            } else if (key < array[middle]) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    private static int binarySearchRecursive(int[] array, int left, int right, int key) {
+        int middle = (left + right) / 2;
+        if (left > right) {
+            return -1;
+        } else if (key < array[middle]) {
+            return binarySearchRecursive(array, left, middle - 1, key);
+        } else if (key > array[middle]) {
+            return binarySearchRecursive(array, middle + 1, right, key);
+        } else {
+            return middle;
+        }
     }
 
 }
--- src/main/java/algorithms/training/Training.java
+++ src/main/java/algorithms/training/Training.java
@@ -3,5 +3,7 @@ package algorithms.training;
 public class Training {
     public static void main(String[] args) {
 
+
     }
+
 }
