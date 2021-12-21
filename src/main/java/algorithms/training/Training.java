e5e0365f647bc860bdf1563908d44de68c6d6642
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-12-21 16:37:14 +0300

    added arraylist class

--- src/main/java/algorithms/training/Training.java
+++ src/main/java/algorithms/training/Training.java
@@ -1,9 +1,13 @@
 package algorithms.training;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Training {
     public static void main(String[] args) {
+
+        List<String> arrayList = new ArrayList<>();
 
     }
 }
---
+++ src/main/java/datastructures/myarraylist/MyArrayList.java
@@ -0,0 +1,18 @@
+package datastructures.myarraylist;
+
+public class MyArrayList {
+    /*
+    * Methods:
+    * boolean add(E e)
+    * void add(int index, E e)
+    * E get(int index)
+    * void set(int index, E e)
+    * boolean remove(int index)
+    * boolean remove(E e)
+    * void trimTiSize()
+    * int size()
+    * ensureCapacity(int capacity)
+    * */
+
+
+}
