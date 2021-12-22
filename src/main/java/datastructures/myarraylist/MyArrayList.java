d67bcc4d49db90486775acfd33162f91f4bf5e2b
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-12-22 10:45:21 +0300

    mod MyArrayList

--- src/main/java/datastructures/myarraylist/MyArrayList.java
+++ src/main/java/datastructures/myarraylist/MyArrayList.java
@@ -1,9 +1,53 @@
 package datastructures.myarraylist;
 
-public class MyArrayList {
+public class MyArrayList<T> implements MyList<T> {
 
     private long size;
     private long capacity;
     private Object[] array;
 
+    @Override
+    public boolean add(T element) {
+        return false;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(T element) {
+        return false;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void size() {
+
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
 }
