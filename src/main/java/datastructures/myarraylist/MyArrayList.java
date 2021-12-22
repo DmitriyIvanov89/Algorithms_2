4d655fe3eee28ae0fb979c127174063e3d542a15
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-12-22 10:29:11 +0300

    added List interface

--- src/main/java/datastructures/myarraylist/MyArrayList.java
+++ src/main/java/datastructures/myarraylist/MyArrayList.java
@@ -1,18 +1,9 @@
 package datastructures.myarraylist;
 
 public class MyArrayList {
-    /*
-    * Methods:
-    * boolean add(E e)
-    * void add(int index, E e)
-    * E get(int index)
-    * void set(int index, E e)
-    * boolean remove(int index)
-    * boolean remove(E e)
-    * void trimTiSize()
-    * int size()
-    * ensureCapacity(int capacity)
-    * */
 
+    private long size;
+    private long capacity;
+    private Object[] array;
 
 }
---
+++ src/main/java/datastructures/myarraylist/MyList.java
@@ -0,0 +1,19 @@
+package datastructures.myarraylist;
+
+public interface MyList {
+
+    /*
+    * add(E e)
+    * add(int index,E element)
+    * clear()
+    * isEmpty()
+    * remove(int index)
+    * remove(Object o)
+    * set(int index, E element)
+    * size()
+    * toArray()*/
+
+    
+
+
+}
