65ac5437690eeb3447a1312f3ea3189aed25b80f
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-01-09 15:56:54 +0300

    start arraylist work

--- src/main/java/datastructures/myarraylist/MyArrayList.java
+++ src/main/java/datastructures/myarraylist/MyArrayList.java
@@ -1,6 +1,79 @@
 package datastructures.myarraylist;
 
-public class MyArrayList<T> {
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class MyArrayList<E> implements MyList<E> {
+
+    private int size;
+    private int capacity;
+    private final static int DEFAULT_CAPACITY = 10;
+    private E[] array;
 
 
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void add(int index) {
+
+    }
+
+    @Override
+    public boolean add(E element) {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        return false;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+        MyList.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return MyList.super.spliterator();
+    }
 }
