0aca75c3f738110bbc80175c2595b4d882b19f2d
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2022-01-10 10:48:48 +0300

    before pull

--- src/main/java/algorithms/training/Training.java
+++ src/main/java/algorithms/training/Training.java
@@ -11,8 +11,13 @@ import java.util.List;
 public class Training {
     public static void main(String[] args) {
 
+        MyList<Integer> integerMyList = new MyArrayList<>();
 
-        
+        for (int i = 0; i < 10; i++) {
+            integerMyList.add(i);
+        }
+
+        System.out.println("test");
 
     }
 }
