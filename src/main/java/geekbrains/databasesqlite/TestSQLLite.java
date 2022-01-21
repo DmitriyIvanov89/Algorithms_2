1fb8e80da5d0359ad37c8d55b0bd1665a5625268
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-01-21 14:20:14 +0300

    mod example database

---
+++ src/main/java/geekbrains/databasesqlite/TestSQLLite.java
@@ -0,0 +1,14 @@
+package geekbrains.databasesqlite;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class TestSQLLite {
+
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void main(String[] args) {
+
+    }
+}
