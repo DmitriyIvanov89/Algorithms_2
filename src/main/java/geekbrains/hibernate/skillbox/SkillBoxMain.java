3e83d51d97e40202e51618dae9fc3dd3e10d7623
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-02-11 16:06:37 +0300

    added new methos ShowProductsByPerson

--- src/main/java/geekbrains/hibernate/skillbox/SkillBoxMain.java
+++ src/main/java/geekbrains/hibernate/skillbox/SkillBoxMain.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class SkillBoxMain {
     public static void main(String[] args) {
 
@@ -20,22 +22,32 @@ public class SkillBoxMain {
         try {
 
             // create new Order
-            session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            Person person = (Person) session.createQuery("FROM Person p WHERE p.id = :id").setParameter("id", 6L).uniqueResult();
-            Product product = (Product) session.createQuery("FROM Product p WHERE p.id = :id").setParameter("id", 1L).uniqueResult();
+//            session = sessionFactory.getCurrentSession();
+//            session.beginTransaction();
+//            Person person = (Person) session.createQuery("FROM Person p WHERE p.id = :id").setParameter("id", 1L).uniqueResult();
+//            Product product = (Product) session.createQuery("FROM Product p WHERE p.id = :id").setParameter("id", 5L).uniqueResult();
+//
+//            OrderId orderId = new OrderId();
+//            orderId.setPersonId(person.getId());
+//            orderId.setProductId(product.getId());
+//            Order order = new Order();
+//            order.setOrderId(orderId);
+//            order.setPrice(product.getPrice());
+//
+//            session.save(order);
+//            session.getTransaction().commit();
+//
+//            System.out.println("Create new order: " + order.toString());
+            // ShowProductsByPerson
+//            session = sessionFactory.getCurrentSession();
+//            session.beginTransaction();
+//            Person person = (Person) session.createQuery("FROM Person p WHERE p.id = :id").setParameter("id", 1L).uniqueResult();
+//            List<Order> orders = person.getOrders();
+//            for (Order order : orders) {
+//                System.out.println(order);
+//            }
 
-            OrderId orderId = new OrderId();
-            orderId.setPersonId(person.getId());
-            orderId.setProductId(product.getId());
-            Order order = new Order();
-            order.setOrderId(orderId);
-            order.setPrice(product.getPrice());
 
-            session.save(order);
-            session.getTransaction().commit();
-
-            System.out.println("Create new order: " + order.toString());
 
         } finally {
             session.close();
