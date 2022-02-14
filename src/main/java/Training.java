0e42f6ea9cd5dd0e11ce8082e673f83f332803f2
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2022-02-14 16:09:44 +0300

    mod project

--- src/main/java/Training.java
+++ src/main/java/Training.java
@@ -1,7 +1,14 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Training {
 
     public static void main(String[] args) {
-    	System.out.println("Hello eclipse");
+
+        DateFormat dateFormat = new SimpleDateFormat("hh:mm a dd/MM/yyyy");
+        Date date = new Date();
+        System.out.println("Today is: " + dateFormat.format(date));
+
     }
 }
