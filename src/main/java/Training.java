d268c3c6f03b1187c742f5b1bd873b3e55c394be
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2022-02-15 15:33:31 +0300

    next task

--- src/main/java/Training.java
+++ src/main/java/Training.java
@@ -6,9 +6,8 @@ public class Training {
 
     public static void main(String[] args) {
 
-        DateFormat dateFormat = new SimpleDateFormat("hh:mm a dd/MM/yyyy");
-        Date date = new Date();
-        System.out.println("Today is: " + dateFormat.format(date));
+
+
 
     }
 }
